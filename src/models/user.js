6291c18e5331ba59551df09ecf563a915df75d92
NomaBouh <thbo62126@eleve.isep.fr>
2023-12-04 16:27:13 -0500

    add user

---
+++ src/models/user.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    location: { type: String, require: true },
+    postcode: { type: String, require: true },
+    city: { type: String, require: true },
+    email: { type: String, require: true },
+    Solde: { type: Number, default: 0 },
+    // Autres champs pertinents pour un utilisateur
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
