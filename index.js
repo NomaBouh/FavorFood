880b6b90d3a1dddf6d435a08b51204315fff23ce
NomaBouh <thbo62126@eleve.isep.fr>
2023-12-04 16:27:13 -0500

    commit 0.0.1

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+const express = require('express');
+const http = require('http');
+
+const app = express();
+const server = http.createServer(app);
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+	console.log(`Server running on port ${port}`);
+})
